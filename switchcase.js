32cdb1f4858c32f3bcb3ae5c2ea37911dd94d4a9
Sravanthi <singamsravanthi02@gmail.com>
2026-05-04 14:23:22 +0530

    SwitchCase

---
+++ switchcase.js
@@ -0,0 +1,21 @@
+let num1=45;
+let num2=30;
+let operator="*";
+switch(operator)
+{
+    case '+':
+        console.log(num1+num2);
+        break; 
+    case '-':
+        console.log(num1-num2);
+        break;
+    case '*':
+        console.log(num1*num2);
+        break;
+    case '/':
+        console.log(num1/num2);
+        break;
+    default:
+        console.log("Invalid operator");
+
+}
